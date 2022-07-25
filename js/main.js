6aec072ebc748836ab628f0a29159779a47350c3
CarolPY <carolperezyupanqui@gmail.com>
2022-07-25 19:28:24 -0300

    agregacion de index, script y css

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var nombre = "Carol"
+let apellido = "Perez"
+
+
+
+//declarar variable es = usar var, let o const
+// var LOQUESEA
+// let TELEFONO (el valor de este se puede cambiar cuando quieras)
+// cont anioNacimiento (el valor de este NO se cambia nunca xq no quiere)
+
+//VALORES  (para asignar valor es =) 
+// dos tipos =
+//NUMBER (entero o decimal)
+// STRING (texto)
